a3506772c1e8aabecee223ff5c8e26f1c86faf59
boxplay <903199865@qq.com>
2019-06-21 00:57:01 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Admin from '@/admin/components/Backend'
-import Web from '@/index/components/Index'
-import Mob from '@/mob/components/Mob'
-import Webadmin from '@/admin/components/Backend2'
+// import Admin from '@/admin/components/Backend'
+// import Web from '@/index/components/Index'
+// import Mob from '@/mob/components/Mob'
+// import Webadmin from '@/admin/components/Backend2'
+const Admin = () => import('@/admin/components/Backend')
+const Web = () => import('@/index/components/Index')
+const Mob = () => import('@/mob/components/Mob')
+const Webadmin = () => import('@/admin/components/Backend2')
+
 
 Vue.use(Router)
 
