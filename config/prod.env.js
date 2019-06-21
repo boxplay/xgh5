7bca070d78a9810d119760eeb0577b16dbe08870
boxplay <903199865@qq.com>
2019-06-21 16:44:43 +0800

    init

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,5 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+	GA: '"UA-100002932-38"'
 }
--- src/main.js
+++ src/main.js
@@ -6,13 +6,26 @@ import router from './router'
 import axios from 'axios'
 Vue.prototype.$axios = axios
 import '@/assets/css/common.css'
-
-import ElementUI from 'element-ui'
+import VueAnalytics from 'vue-analytics'
+import {Form,FormItem,Upload,Input} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(ElementUI)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Upload)
+Vue.use(Input)
 Vue.config.productionTip = false
-
+// GA初始化
+if (process.env.GA) {
+  Vue.use(VueAnalytics, {
+    id: process.env.GA, // 从配置中读取
+    disableScriptLoader: true, // 必须在html中完成初始化，这里显式禁止去下载ga脚本
+    router, // 确保路由切换时可以自动统计
+    autoTracking: {
+      pageviewOnLoad: false // 当通过网址进来时已经GA在初始化时就发起一次pageview的统计，这里不要重复统计
+    }
+  })
+}
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
