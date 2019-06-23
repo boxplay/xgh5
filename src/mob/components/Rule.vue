1d1ae80bc9c1e755a4f91281da683a257922861a
boxplay <903199865@qq.com>
2019-06-23 17:44:52 +0800

    如果调整图片则去掉style 和增加 滚动监听替换导航

--- src/mob/components/Rule.vue
+++ src/mob/components/Rule.vue
@@ -1,7 +1,15 @@
 <template>
 	<div class="main" ref='main'>
 		<div class="imgBoxImg normalDiv">
-			<img src="https://xgh5.someet.cc/rule.jpg" alt="" width="100%">
+			<div id='ruleBox'>
+				<img src="https://xgh5.someet.cc/rule_back.jpeg" alt="" width="100%">
+			</div>
+			<div id='ruleBoxWhite'>
+				<img src="https://xgh5.someet.cc/white_back.png" alt="" width="100%">
+			</div>
+			<div id="ruleBoxImgDiv">
+				<img src="https://xgh5.someet.cc/rule_text.png" alt="" width="100%">
+			</div>
 		</div>
 	</div>
 </template>
@@ -17,4 +25,29 @@
 </script>
 
 <style>
+	.main{
+		width: 100%;
+		max-width: 700px;
+		margin: 0 auto;
+	}
+	#ruleBox{
+		position: absolute;
+		left:0;
+		right:0;
+		margin: 0 auto;
+	}
+	#ruleBoxWhite{
+		position: absolute;
+		left:0;
+		right:0;
+		margin: 0 auto;
+		margin: 3%;
+	}
+	#ruleBoxImgDiv{
+		position: absolute;
+		left:0;
+		right:0;
+		margin: 0 auto;
+		margin: 8%;
+	}
 </style>
