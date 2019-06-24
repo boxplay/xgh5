9e23d62ad58a4214982e41dcecec4055885514bd
boxplay <903199865@qq.com>
2019-06-25 02:24:54 +0800

    更换图片

--- php/getToken.php
+++
@@ -1,8 +0,0 @@
-<?php
-ini_set("display_errors", "On");
-error_reporting(E_ALL);
-require_once "jssdk.php";
-$url = $_POST['reqUrl'];
-$jssdk = new JSSDK("wxb83633a7c4d934ab", "7cb05989b511ae6e369fa724b5b4d5e3",$url);
-echo json_encode($jssdk->getSignPackage());
-?>
