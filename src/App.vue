b4e965096b0a306949f52ad69cab786cbb2edddd
boxplay <903199865@qq.com>
2019-06-25 11:31:43 +0800

    调整位置

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,20 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+	data(){
+		return {
+			
+		}
+	},
+	methods:{
+		_isMobile(){
+			let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
+			// localStorage.setItem('isiphone',flag)
+			localStorage.setItem('ismobile',flag?1:0)
+			return flag;
+		}
+	}
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,7 +4,30 @@ import App from './App'
 import router from './router'
 Vue.prototype.$axios = axios
 import '@/assets/css/common.css'
+router.beforeEach((to, from, next) => {
+	var ismobile = localStorage.getItem('ismobile');
+	if(ismobile == null){
+		let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
 
+		ismobile = flag ? 1 : 0
+	}
+	// 电脑
+	if (ismobile == 0) {
+		if (to.path != '/') {
+			next('/')
+		} else {
+			next();
+		}
+	}
+	// 手机
+	if (ismobile == 1) {
+		if (to.path != '/mob') {
+			next('/mob')
+		} else {
+			next();
+		}
+	}
+})
 Vue.config.productionTip = false
 Vue.use(window.VueVideoPlayer)
 Vue.use(window.VueAwesomeSwiper)
